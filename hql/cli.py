d2f90293d82d333d346821ba48e5ead45b7e94a2
Steven Frieson <sfrieson@gmail.com>
2018-02-11 18:20:50 -0500

    Playing around with click

--- hql/cli.py
+++ hql/cli.py
@@ -1,10 +1,22 @@
 import click
 
 
-@click.command()
-@click.option('--as-cowboy', '-c', is_flag=True, help='Greet as a cowboy.')
-@click.argument('name', default='world', required=False)
-def main(name, as_cowboy):
+# @click.option('--as-cowboy', '-c', is_flag=True, help='Greet as a cowboy.')
+# @click.argument('name', default='world', required=False)
+@click.group()
+def main():
     """Query HTML files."""
-    greet = 'Howdy' if as_cowboy else 'Hello'
-    click.echo('{0}, {1}.'.format(greet, name))
+    pass
+
+@click.command()
+def something():
+    """Do something."""
+    click.echo('You got something to happen.')
+
+@click.command()
+def something_else():
+    """Do something else."""
+    click.echo('You got something else to happen.')
+
+main.add_command(something)
+main.add_command(something_else)
