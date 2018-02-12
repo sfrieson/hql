95df5dbe8c573d3441614f2cc551f34d574f7c6d
Steven Frieson <sfrieson@gmail.com>
2018-02-11 19:29:35 -0500

    Adds query

--- hql/query.py
+++ hql/query.py
@@ -1,2 +1,10 @@
-def main (url):
+"""Query and set up the environment"""
+
+import requests
+
+
+def main(url):
     print('File Querying: ' + url)
+    response = requests.get(url)
+    print('Status: %d' % response.status_code)
+    print(response.text)
