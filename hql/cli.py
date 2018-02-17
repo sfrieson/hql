e969e0b7e6d0a8f4152359f3abdcce7f47c30380
Steven Frieson <sfrieson@gmail.com>
2018-02-17 18:05:26 -0500

    Changes query name to data

--- hql/cli.py
+++ hql/cli.py
@@ -1,5 +1,6 @@
 import click
-import hql.query as query
+import hql.data as data
+
 import re
 
 message_set_url = ('Please set the a url with `url` and '
@@ -12,7 +13,7 @@ def main(url):
     """Query HTML files."""
     response = 'No response yet.\n%s' % message_set_url
     if url:
-        response = query.main(url)
+        response = data.main(url)
 
     user_input = ''
     while user_input != 'exit':
@@ -29,7 +30,7 @@ url - to retrieve and set the current url
             """)
         elif user_input == 'refresh':
             if url:
-                response = query.main(url)
+                response = data.main(url)
             else:
                 print("No url is set.\n%s" % message_set_url)
         elif user_input == 'response':
